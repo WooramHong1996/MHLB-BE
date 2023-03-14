d20d1a46db36d9edb731b30d6b988e89ae9d0892
jan <samjan1127@gmail.com>
2023-03-14 15:42:19 +0900

    기본 세팅

---
+++ src/main/java/com/gigajet/mhlb/domain/user/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.gigajet.mhlb.domain.user.controller;
+
+import com.gigajet.mhlb.common.dto.SendMessageDto;
+import com.gigajet.mhlb.domain.user.dto.UserRequestDto;
+import com.gigajet.mhlb.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/duplicate-email")
+    public ResponseEntity<SendMessageDto> duplicateEmail(@RequestBody String email) {
+        return userService.duplicateEmail(email);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<SendMessageDto> register(@RequestBody UserRequestDto.Register registerDto) {
+        return userService.register(registerDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<SendMessageDto> login(@RequestBody UserRequestDto.Login loginDto, HttpServletResponse response) {
+        return userService.login(loginDto, response);
+    }
+
+}
---
+++ src/main/java/com/gigajet/mhlb/domain/user/dto/UserRequestDto.java
@@ -0,0 +1,33 @@
+package com.gigajet.mhlb.domain.user.dto;
+
+import lombok.Getter;
+
+public class UserRequestDto {
+
+    @Getter
+    public static class Register {
+
+        private String email;
+
+        private String password;
+
+        private String userName;
+
+        private String userImage;
+
+        private String userJob;
+
+        private String userDesc;
+
+    }
+
+    @Getter
+    public static class Login {
+
+        private String email;
+
+        private String password;
+
+    }
+
+}
