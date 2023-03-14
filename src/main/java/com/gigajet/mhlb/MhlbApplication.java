9410aed1962a382cc6db443cf7a3303f0e2bd33e
jan <samjan1127@gmail.com>
2023-03-14 10:52:46 +0900

    init

---
+++ src/main/java/com/gigajet/mhlb/MhlbApplication.java
@@ -0,0 +1,13 @@
+package com.gigajet.mhlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhlbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhlbApplication.class, args);
+	}
+
+}
