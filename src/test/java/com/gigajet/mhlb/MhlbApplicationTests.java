1f7550efea6714cd224be40bdccada0e23939d29
jan <samjan1127@gmail.com>
2023-03-14 11:03:20 +0900

    init test

---
+++ src/test/java/com/gigajet/mhlb/MhlbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigajet.mhlb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhlbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
