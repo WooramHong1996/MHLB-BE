371d5aac454bbee8d596b4875fc3c766ce668845
Galmaeki <kjh09800@gmail.com>
2023-03-15 23:51:27 +0900

    Fix: 코드 가독성 향상

--- src/main/java/com/gigajet/mhlb/domain/managing/service/ManagingService.java
+++ src/main/java/com/gigajet/mhlb/domain/managing/service/ManagingService.java
@@ -126,7 +126,7 @@ public class ManagingService {
     private WorkspaceUser checkRole(User user, Long id) {
         WorkspaceUser workspaceUser = workspaceUserRepository.findByUserAndWorkspaceId(user, id).orElseThrow(() -> new CustomException(ErrorCode.PERMISSION_DINED));
 
-        if (!(workspaceUser.getRole() == WorkspaceUserRole.MANAGER || workspaceUser.getRole() == WorkspaceUserRole.ADMIN)) {
+        if (workspaceUser.getRole() == WorkspaceUserRole.MEMBER) {
             throw new CustomException(ErrorCode.PERMISSION_DINED);
         }
 
