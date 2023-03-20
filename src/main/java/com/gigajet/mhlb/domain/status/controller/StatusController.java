5adb2fa276f5b47fadbb47627ddcf818788a4c87
Galmaeki <kjh09800@gmail.com>
2023-03-20 17:05:25 +0900

    faet: Status sse기능 구현

--- src/main/java/com/gigajet/mhlb/domain/status/controller/StatusController.java
+++ src/main/java/com/gigajet/mhlb/domain/status/controller/StatusController.java
@@ -1,12 +1,19 @@
 package com.gigajet.mhlb.domain.status.controller;
 
+import com.gigajet.mhlb.common.dto.SendMessageDto;
+import com.gigajet.mhlb.common.util.SuccessCode;
 import com.gigajet.mhlb.domain.status.dto.StatusRequestDto;
 import com.gigajet.mhlb.domain.status.dto.StatusResponseDto;
 import com.gigajet.mhlb.domain.status.service.StatusService;
+import com.gigajet.mhlb.domain.status.sse.StatusSseHandler;
 import com.gigajet.mhlb.security.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -15,10 +22,16 @@ import java.util.List;
 @RequestMapping("/api/status")
 public class StatusController {
     private final StatusService statusService;
+    private final StatusSseHandler sseHandler;
 
     @PostMapping
-    public String stausUpdate(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody StatusRequestDto statusRequestDto) {
-        return statusService.statusUpdate(userDetails.getUser(), statusRequestDto);
+    public ResponseEntity<SendMessageDto> stausUpdate(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody StatusRequestDto statusRequestDto) {
+        StatusResponseDto dto = statusService.statusUpdate(userDetails.getUser(), statusRequestDto);
+        List<Long> workspaceList = statusService.getWorkspaceList(userDetails.getUser());
+        for (Long id : workspaceList) {
+            sseHandler.statusChanged(id,dto);
+        }
+        return SendMessageDto.toResponseEntity(SuccessCode.STATUS_CHANGED);
     }
 
     @GetMapping
@@ -29,5 +42,12 @@ public class StatusController {
     @GetMapping("/{id}")//id워크스페이스에 속한 유저들의 상태를 전부 가져옴
     public List getWorkspacePeople(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
         return statusService.getWorkspacePeople(userDetails.getUser(), id);
+    }
+
+    @GetMapping(value = "/{id}/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)//sse 시작 요청
+    public ResponseEntity<SseEmitter> connect(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id){
+        SseEmitter emitter = new SseEmitter();
+        sseHandler.add(id);
+        return ResponseEntity.ok(emitter);
     }
 }
---
+++ src/main/java/com/gigajet/mhlb/domain/status/sse/StatusSseHandler.java
@@ -0,0 +1,61 @@
+package com.gigajet.mhlb.domain.status.sse;
+
+import com.gigajet.mhlb.domain.status.dto.StatusResponseDto;
+import com.gigajet.mhlb.exception.CustomException;
+import com.gigajet.mhlb.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class StatusSseHandler {
+    //워크스페이스 id별로 구분해야함
+    private final ConcurrentHashMap<Long, List<SseEmitter>> emitters = new ConcurrentHashMap();
+
+    public SseEmitter add(Long workspaceId) {
+        List<SseEmitter> emitterList = emitters.get(workspaceId);
+
+        if (emitterList == null) {
+            emitterList = new ArrayList<>();
+            emitters.put(workspaceId, emitterList);
+        }
+
+        SseEmitter emitter = new SseEmitter();
+
+        emitterList.add(emitter);
+
+        emitter.onCompletion(() -> {//emitter 종료시 로직
+            List<SseEmitter> list = emitters.get(workspaceId);
+            list.remove(emitter);
+            if (list.isEmpty()) {
+                emitters.remove(workspaceId);
+            }
+        });
+
+        emitter.onTimeout(() -> new IllegalAccessError("타임아웃 시간 안정해놔서 안터짐...아마"));
+
+        return emitter;
+    }
+
+    public void statusChanged(Long workspaceId, StatusResponseDto dto) {
+        List<SseEmitter> emitterList = emitters.get(workspaceId);
+        if (emitterList == null) {
+            throw new CustomException(ErrorCode.PERMISSION_DINED);
+        }
+        for (SseEmitter emitter : emitterList) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("statusChanged")
+                        .data(dto));
+            } catch (IOException e) {
+                emitterList.remove(emitter);
+            }
+        }
+    }
+}
