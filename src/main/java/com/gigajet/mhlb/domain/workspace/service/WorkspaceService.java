f7b5d0e3ed26fc7cb2131c1c91f7d8efd206f794
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-03-21 20:44:34 +0900

    Refactor: 코드컨벤션에 따라 refactoring

--- src/main/java/com/gigajet/mhlb/domain/workspace/service/WorkspaceService.java
+++ src/main/java/com/gigajet/mhlb/domain/workspace/service/WorkspaceService.java
@@ -70,7 +70,6 @@ public class WorkspaceService {
 
         workspaceUserRepository.save(workspaceUser);
 
-
         workspaceOrderRepository.save(workspaceOrder);
 
         return new WorkspaceResponseDto.CreateResponse(workspace);
