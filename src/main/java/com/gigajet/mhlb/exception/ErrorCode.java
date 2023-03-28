553072f5f10e8fa7f7eb1d615390b643f6872f8a
Galmaeki <kjh09800@gmail.com>
2023-03-28 21:22:52 +0900

    Refactor: Status 요청부분 요구사항에 맞게 변경

--- src/main/java/com/gigajet/mhlb/exception/ErrorCode.java
+++ src/main/java/com/gigajet/mhlb/exception/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode {
     ALREADY_INVITED(BAD_REQUEST, "already invited"),
     INVALID_CODE(BAD_REQUEST, "invalid code"),
     STATUS_NOT_CHANGED(BAD_REQUEST, "같은 상태로는 변경할 수 없습니다."),
+    STATUS_NOT_EXIST(BAD_REQUEST, "존재하지 않는 상태값 입니다"),
     INVALID_INVITATION(BAD_REQUEST, "Invalid invitation"),
 
 
