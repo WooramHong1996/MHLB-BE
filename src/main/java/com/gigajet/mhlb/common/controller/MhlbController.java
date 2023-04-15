cd34ae2a190fb111499d067124eb7ee64ead345b
jan <samjan1127@gmail.com>
2023-04-15 23:27:51 +0900

    Feat: 로드밸런서 상태 검사용 POST 매핑 API

--- src/main/java/com/gigajet/mhlb/common/controller/MhlbController.java
+++ src/main/java/com/gigajet/mhlb/common/controller/MhlbController.java
@@ -2,13 +2,19 @@ package com.gigajet.mhlb.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MhlbController {
 
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index1() {
+        return new ModelAndView("index");
+    }
+
+    @PostMapping("/")
+    public ModelAndView index2() {
         return new ModelAndView("index");
     }
 }
